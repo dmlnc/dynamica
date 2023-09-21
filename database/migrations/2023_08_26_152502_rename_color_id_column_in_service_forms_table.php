be0b9e33c0ffba904b98a6d3979aac7f34ef4ca3
Dmitrii <36060841+dmlnc@users.noreply.github.com>
2023-09-21 16:28:27 +0300

    Update 2023_08_26_152502_rename_color_id_column_in_service_forms_table.php

--- database/migrations/2023_08_26_152502_rename_color_id_column_in_service_forms_table.php
+++ database/migrations/2023_08_26_152502_rename_color_id_column_in_service_forms_table.php
@@ -16,7 +16,7 @@ class RenameColorIdColumnInServiceFormsTable extends Migration
         Schema::table('service_forms', function (Blueprint $table) {
             $table->dropForeign(['color_id']); // Удаление внешнего ключа
             $table->renameColumn('color_id', 'color'); // Переименование столбца
-            $table->string('color')->change(); // Изменение типа данных на строку
+            // $table->string('color')->change(); // Изменение типа данных на строку
             $table->string('run'); // Добавление нового столбца 'run'
             $table->text('recommendation')->nullable();
             // has_lkp -> bool
@@ -33,7 +33,7 @@ class RenameColorIdColumnInServiceFormsTable extends Migration
     {
         Schema::table('service_forms', function (Blueprint $table) {
             $table->renameColumn('color', 'color_id');
-            $table->integer('color_id')->change()->unsigned();
+            // $table->integer('color_id')->change()->unsigned();
             $table->foreign('color_id')->references('id')->on('colors')->onDelete('cascade');
             $table->dropColumn('run');
             $table->dropColumn('recommendation'); 
